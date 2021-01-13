31ae68c182bbe47c160bae0cb91c3ec6f2271d3d
Adalid <walasuno@gmail.com>
2021-01-13 17:55:14 -0500

    add D

--- src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
+++ src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
@@ -45,7 +45,7 @@ class ValidatorTests {
 
 		LocaleContextHolder.setLocale(Locale.ENGLISH);
 		Person person = new Person();
-		person.setFirstName("");
+		person.setFirstName("D");
 		person.setLastName("smith");
 
 		Validator validator = createValidator();
